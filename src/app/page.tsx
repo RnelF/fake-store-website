d640fbe1b52cc0acd906adb993df3fbe4a1177a5
Arnel Francia <nelffff@gmail.com>
2024-12-13 20:18:05 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 import Image from "next/image";
 
-export default function Home() {
-  return <main>Home</main>;
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes");
+
+  return result.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8"></div>
+    </main>
+  );
 }
