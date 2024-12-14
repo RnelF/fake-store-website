0015845f5a372c1f4eddb272d81cbe53d9c88522
Arnel Francia <nelffff@gmail.com>
2024-12-14 20:11:21 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 interface Recipe {
   title: string;
   image: string;
@@ -19,7 +28,25 @@ export default async function Home() {
   const recipes = await getRecipes();
   return (
     <main>
-      <div className="grid grid-cols-3 gap-8"></div>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id}>
+            <CardHeader>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook</CardDescription>
+              </div>
+              <CardContent>
+                <p>{recipe.description}</p>
+              </CardContent>
+            </CardHeader>
+            <CardFooter>
+              <button>View Recipe</button>
+              {recipe.vegan && <p>Vegan</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 }
