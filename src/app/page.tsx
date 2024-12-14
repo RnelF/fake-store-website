18316a377e48e0dcd04537dc57edeba6493dcb6a
Arnel Francia <nelffff@gmail.com>
2024-12-14 20:41:36 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,10 @@ export default async function Home() {
         {recipes.map((recipe) => (
           <Card key={recipe.id} className="flex flex-col justify-between">
             <CardHeader className="flex-row gap-4 items center">
-              {/*Avatar here*/}
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} alt="recipe img" />
+                <AvatarFallback>{recipe.title.slice(0, 2)}</AvatarFallback>
+              </Avatar>
               <div>
                 <CardTitle>{recipe.title}</CardTitle>
                 <CardDescription>{recipe.time} mins to cook</CardDescription>
@@ -45,8 +48,8 @@ export default async function Home() {
             </CardContent>
 
             <CardFooter className="flex justify-between">
-              <button>View Recipe</button>
-              {recipe.vegan && <p>Vegan</p>}
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
             </CardFooter>
           </Card>
         ))}
