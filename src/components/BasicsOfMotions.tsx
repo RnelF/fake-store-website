ec9f1e7936b643f0da93f7253546b7e1b8c389b2
Arnel Francia <nelffff@gmail.com>
2024-12-18 19:51:08 +0800

    framer motion basics

--- src/components/BasicsOfMotions.tsx
+++ src/components/BasicsOfMotions.tsx
@@ -1,18 +1,30 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 export default function BasicsOfMotions() {
+  const [isVisible, setIsVisible] = useState(true);
   return (
     <div className="grid justify-center items-center h-[100vh] gap-4">
-      <motion.div
-        initial={{ rotate: "0deg" }}
-        animate={{ rotate: "180deg" }}
-        transition={{
-          duration: 1,
-          ease: "backInOut",
-        }}
-        className="w-36 h-36 bg-black"
-      ></motion.div>
+      <button
+        onClick={() => setIsVisible(!isVisible)}
+        className="example-button"
+      >
+        Show/Hide
+      </button>
+      <AnimatePresence>
+        {isVisible && (
+          <motion.div
+            exit={{ rotate: "0deg" }}
+            initial={{ rotate: "0deg" }}
+            animate={{ rotate: "180deg" }}
+            transition={{
+              duration: 1,
+              ease: "backInOut",
+            }}
+            className="w-36 h-36 bg-black"
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
