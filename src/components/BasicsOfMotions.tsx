02f2755085e0644992c46c117030bb25e807f715
Arnel Francia <nelffff@gmail.com>
2024-12-21 19:33:45 +0800

    updated

--- src/components/BasicsOfMotions.tsx
+++ src/components/BasicsOfMotions.tsx
@@ -5,21 +5,27 @@ export default function BasicsOfMotions() {
   const [isVisible, setIsVisible] = useState(true);
   return (
     <div className="grid justify-center items-center h-[100vh] gap-4">
-      <button
+      <motion.button
         onClick={() => setIsVisible(!isVisible)}
         className="example-button"
+        layout
       >
         Show/Hide
-      </button>
-      <AnimatePresence>
+      </motion.button>
+      <AnimatePresence mode="popLayout">
         {isVisible && (
           <motion.div
-            exit={{ rotate: "0deg" }}
-            initial={{ rotate: "0deg" }}
-            animate={{ rotate: "180deg" }}
+            exit={{ rotate: "0deg", scale: 0, y: 0 }}
+            initial={{ rotate: "0deg", scale: 0, y: 0 }}
+            animate={{
+              rotate: "180deg",
+              scale: 1,
+              y: [0, 150, -150, -150, 0],
+            }}
             transition={{
               duration: 1,
               ease: "backInOut",
+              times: [0, 0.25, 0.5, 0.85, 1],
             }}
             className="w-36 h-36 bg-black"
           ></motion.div>
